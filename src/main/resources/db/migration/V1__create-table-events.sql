668e64de30a19e43508c200c200470692c376343
ryuzera <ryugfx2@gmail.com>
2025-03-27 16:34:47 -0300

    Criação dos migrations para criação das tabelas

---
+++ src/main/resources/db/migration/V1__create-table-events.sql
@@ -0,0 +1,7 @@
+CREATE TABLE events (
+    id VARCHAR(255) NOT NULL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    datails VARCHAR(255) NOT NULL,
+    slug VARCHAR(255) NOT NULL,
+    maximum_attendees INTEGER NOT NULL
+);
