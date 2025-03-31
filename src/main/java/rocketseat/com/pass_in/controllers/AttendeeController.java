0b07c989c2953afecd73844a5ac17de6f9628c59
ryuzera <ryugfx2@gmail.com>
2025-03-31 17:09:32 -0300

    Criação do endpoint para exibição do badge

--- src/main/java/rocketseat/com/pass_in/controllers/AttendeeController.java
+++ src/main/java/rocketseat/com/pass_in/controllers/AttendeeController.java
@@ -1,16 +1,24 @@
 package rocketseat.com.pass_in.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+import rocketseat.com.pass_in.dto.attendee.AttendeeBadgeResponseDTO;
+import rocketseat.com.pass_in.services.AttendeeService;
 
 @RestController
 @RequestMapping("/attendees")
+@RequiredArgsConstructor
 public class AttendeeController {
+    private final AttendeeService attendeeService;
 
-    @GetMapping
-    public ResponseEntity<String> getTest(){
-        return ResponseEntity.ok("Attendee Success!");
+    @GetMapping("/{attendeeId}/badge")
+    public ResponseEntity<AttendeeBadgeResponseDTO> getAttendeeBadge(@PathVariable String attendeeId, UriComponentsBuilder uriComponentsBuilder) {
+        AttendeeBadgeResponseDTO response = this.attendeeService.getAttendeeBadge(attendeeId, uriComponentsBuilder);
+        return ResponseEntity.ok(response);
     }
 }
